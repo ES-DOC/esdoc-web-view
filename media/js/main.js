15ec7a6e9228930126472223e596d6bc82577edd
Mark Morgan <momipsl@ipsl.jussieu.fr>
2017-03-21 17:17:24 +0100

    version bump

--- media/js/main.js
+++ media/js/main.js
@@ -15,7 +15,7 @@ this.window.$ = this.window.$jq = this.jQuery.noConflict();
         title: "ES-DOC Viewer",
 
         // Version.
-        VERSION: '0.9.7.1',
+        VERSION: '0.9.7.2',
 
         // Copyright statement.
         copyrightYear: new Date().getFullYear(),
